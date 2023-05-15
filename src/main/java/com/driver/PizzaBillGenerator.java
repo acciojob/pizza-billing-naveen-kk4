b071c70edb11f1eb8040003d3711ba7403719aa5
naveen-kk4 <naveennig2001@gmail.com>
2023-05-16 02:01:37 +0530

    finall

--- src/main/java/com/driver/PizzaBillGenerator.java
+++ src/main/java/com/driver/PizzaBillGenerator.java
@@ -2,9 +2,14 @@ package com.driver;
 
 public class PizzaBillGenerator implements BillGenerator{
 
-    String bill = "";
+    String bill;
+   /* Base Price Of The Pizza: 300
+    Extra Cheese Added: 80
+    Extra Toppings Added: 70
+    Paperbag Added: 20
+    Total Price: 470*/
     public String Bill(Pizza a){
-         bill+="Base Price Of The Pizza: "+a.basePrice+"\n";
+         bill="Base Price Of The Pizza: "+a.basePrice+"\n";
         if(a.isExtraCheese())bill+="Extra Cheese Added: "+a.cheesePrice+"\n";
         if(a.isExtraToppings())bill+="Extra Toppings Added: "+a.toppingsPrice+"\n";
         if(a.isTakeAway())bill+="Paperbag Added: "+a.takeAwayPrice+"\n";
